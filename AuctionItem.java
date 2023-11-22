cf668fac2accaf38b5e0fbcf0ac20a88212ed9ae
Thomaz Justo <111305190+ThomazJusto@users.noreply.github.com>
2023-11-22 14:53:08 -0300

    Add files via upload

--- AuctionItem.java
+++ AuctionItem.java
@@ -1,3 +1,102 @@
+<<<<<<< HEAD
+// Andrew Shaffer (G00688109)
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuctionItem {
+	private  int itemNumber;
+	private  String itemName;
+	private int highestBid;
+	private String highestBidder;
+	private List<Bid> lista;
+	private boolean paymentApproved;
+	private String sellerName;
+
+	public AuctionItem(int itemNum, String name, String nameSeller)
+	{
+		itemNumber = itemNum;
+		itemName = name;
+		highestBid = 0;
+		highestBidder = null;
+		lista = new ArrayList<>();
+		paymentApproved = false;
+		sellerName = nameSeller;
+	}
+	
+	public AuctionItem(int itemNum, String name)
+	{
+		itemNumber = itemNum;
+		itemName = name;
+		highestBid = 0;
+		highestBidder = null;
+		lista = new ArrayList<>();
+		paymentApproved = false;
+		sellerName = "";
+	}
+
+	
+	public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+    
+	public int getItemNumber()
+	{ return itemNumber;}
+
+	public void setItemNumber(int num)
+	{ itemNumber = num; }
+
+	public String getItemName()
+	{ return itemName;}
+
+	public void setItemName(String name)
+	{ itemName = name; }
+
+	public int getHighestBid()
+	{ return highestBid;}
+
+	public void setHighestBid(int num)
+	{ highestBid = num; }
+
+	public String getHighestBidder()
+	{ return highestBidder;}
+
+	public void setHighestBidder(String name)
+	{ highestBidder = name; }
+	
+	//get maior bidder - return name and valor
+
+	//this method will create a new bid that comes from the server. This is an object type BID, inserted in the list created in this class
+	public void addBid(String nome, int valor) {
+		Bid bid = new Bid(nome, valor);
+		lista.add(bid);
+	}
+	
+	//this method will return the list so it can be printed.
+	public List<Bid> printLista() {
+		return lista;
+	}
+	
+	public int bidsize() {
+		return lista.size();
+	}
+	
+	public boolean isPaymentApproved() {
+		return paymentApproved;
+	}
+	
+	public void approvePayment() {
+		paymentApproved = true;
+	}
+
+	public void setPaymentApproved(boolean b) {
+		paymentApproved = true;
+	}
+
+=======
 // Andrew Shaffer (G00688109)
 import java.util.ArrayList;
 import java.util.List;
@@ -69,4 +168,5 @@ public class AuctionItem {
 		paymentApproved = true;
 	}
 
+>>>>>>> 14ab574bbc45c08e1bbbf215388f04ef68c22672
 }
